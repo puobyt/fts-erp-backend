c3dc2274422d9fea0d93879ccf694bc2a848c283
jishnu anil <jishnuanil255@gmail.com>
2024-11-21 01:24:14 +0530

    changed cores config

--- app.js
+++ app.js
@@ -1,48 +1,54 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-const cors = require('cors')
-var adminRouter = require('./routes/admin')
+const cors = require("cors");
+var adminRouter = require("./routes/admin");
 
-const dbConnect = require('./configs/database')
+const dbConnect = require("./configs/database");
 
 var app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-const PORT = process.env.PORT || 5000
-app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+const PORT = process.env.PORT || 5000;
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  cors({
+    origin: "https://fts-gamma.vercel.app", // Replace with your frontend URL
+    methods: "GET,POST,PUT,DELETE", // Specify allowed HTTP methods
+    allowedHeaders: "Content-Type,Authorization", // Specify allowed headers
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
 dbConnect();
-app.use('/', adminRouter);
+app.use("/", adminRouter);
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
-})
+});
 
 module.exports = app;
