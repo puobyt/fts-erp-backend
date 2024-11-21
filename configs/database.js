24575268781212bd5d8530d2b9dc6f051c9a2c69
jishnu anil <jishnuanil255@gmail.com>
2024-11-21 08:52:42 +0530

    Changed to mongodb atlas

--- configs/database.js
+++ configs/database.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
+// const dbConnect = () => {
+//   mongoose
+//     .connect('mongodb://localhost:27017/EnterpriseApplication')
+//     .then(() => {
+//       console.log("Mongodb connected successfully");
+//     })
+//     .catch((err) => console.log(err.message));
+// };
 const dbConnect = () => {
   mongoose
-    .connect('mongodb://localhost:27017/EnterpriseApplication')
+    .connect('mongodb+srv://yanuhiwii:7pS6GIuIuy40NyLu@clusterfts.wqv73.mongodb.net/FtsCrm?retryWrites=true&w=majority&appName=ClusterFTS')
     .then(() => {
       console.log("Mongodb connected successfully");
     })
